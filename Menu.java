bcaaf128818265ac36828ddb261074973902406c
Juts <Jutslegameur@gmail.com>
2016-07-10 20:01:15 +0200

    importe PlayerJoinEvent

--- Menu.java
+++ Menu.java
@@ -9,6 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
