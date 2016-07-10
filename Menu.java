baeb32bd801a124d4d17a99a2fb5de4551650675
Juts <Jutslegameur@gmail.com>
2016-07-10 17:57:48 +0200

    encadre les méthodes avec des blocs try { } catch()

--- Menu.java
+++ Menu.java
@@ -56,41 +56,47 @@ public class Menu implements Listener {
 	
 	@EventHandler
 	public void onClickCompass(PlayerInteractEvent e) {
-		if(e.getItem().getType() == Material.COMPASS) {
-			Player p = e.getPlayer();
-			if(e.getAction() == Action.RIGHT_CLICK_AIR) {
-				Inventory inv = Bukkit.createInventory(null, 18, "Menu de navigation");
-				ItemStack banner = new ItemStack(Material.BANNER, 1, (byte) 5);
-				ItemMeta bannerM = banner.getItemMeta();
-				bannerM.setDisplayName(ChatColor.YELLOW + "The Towers !");
-				banner.setItemMeta(bannerM);
-				
-				inv.setItem(5, banner);
-				
-				
-				p.openInventory(inv);
+		try {
+			if(e.getItem().getType() == Material.COMPASS) {
+				Player p = e.getPlayer();
+				if(e.getAction() == Action.RIGHT_CLICK_AIR) {
+					Inventory inv = Bukkit.createInventory(null, 18, "Menu de navigation");
+					ItemStack banner = new ItemStack(Material.BANNER, 1, (byte) 5);
+					ItemMeta bannerM = banner.getItemMeta();
+					bannerM.setDisplayName(ChatColor.YELLOW + "The Towers !");
+					banner.setItemMeta(bannerM);
+					
+					inv.setItem(5, banner);
+					
+					
+					p.openInventory(inv);
+				}
 			}
+		} catch (NullPointerException exc) {
+			
 		}
-		
 	}
 	
 	@EventHandler
 	public void onClickSword(PlayerInteractEvent e) {
-		if(e.getItem().getType() == Material.DIAMOND_SWORD) {
-			Player p = e.getPlayer();
-			if(e.getAction() == Action.RIGHT_CLICK_AIR) {
-				Inventory inv = Bukkit.createInventory(null, 18, "Menu des kits");
-				ItemStack woodsword = new ItemStack(Material.WOOD_SWORD, 1);
-				ItemMeta woodswordM = woodsword.getItemMeta();
-				woodswordM.setDisplayName(ChatColor.YELLOW + "Par défaut");
-				woodsword.setItemMeta(woodswordM);
-				
-				inv.setItem(5, woodsword);
-				
-				p.openInventory(inv);
+		try { 
+			if(e.getItem().getType() == Material.DIAMOND_SWORD) {
+				Player p = e.getPlayer();
+				if(e.getAction() == Action.RIGHT_CLICK_AIR) {
+					Inventory inv = Bukkit.createInventory(null, 18, "Menu des kits");
+					ItemStack woodsword = new ItemStack(Material.WOOD_SWORD, 1);
+					ItemMeta woodswordM = woodsword.getItemMeta();
+					woodswordM.setDisplayName(ChatColor.YELLOW + "Par dÃ©faut");
+					woodsword.setItemMeta(woodswordM);
+					
+					inv.setItem(5, woodsword);
+					
+					p.openInventory(inv);
+				}
 			}
+		} catch (NullPointerException exc) {
+			
 		}
-		
 	}
 	
 }
